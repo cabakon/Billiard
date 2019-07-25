de37188a89b506b629b37d04b239f770d15eda1b
cabakon <ondrej@cabak.cz>
2019-07-25 19:43:14 +0200

    project setup

---
+++ Assets/Table/Summonofredball.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Summonofredball : MonoBehaviour
+{
+    public GameObject Redball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Instantiate(Redball, new Vector3(0f, 1.08f, 11f), transform.rotation);
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Table/WhiteBall.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class WhiteBall : MonoBehaviour
+{
+    public GameObject whiteball;
+    Ray ray;
+    RaycastHit hit;
+    Rigidbody rb;
+    public float punchpower;
+
+    // Start is called before the first frame update
+    void Start()
+    {           
+    }
+    private void FixedUpdate()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+               
+                if (hit.collider.name == "White ball")
+                {
+                    hit.transform.GetComponent<Rigidbody>().AddForce(ray.direction * punchpower);
+                }
+            }
+        }
+    }
+}
