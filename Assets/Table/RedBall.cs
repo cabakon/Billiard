7fc01186df344373c2b7d14a10d64a4eeb0109a0
cabakon <ondrej@cabak.cz>
2019-07-25 19:47:34 +0200

    hlaska ahoj

--- Assets/Table/RedBall.cs
+++ Assets/Table/RedBall.cs
@@ -7,7 +7,7 @@ public class RedBall : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Ahoj ja jsem cervena koule");
     }
 
     // Update is called once per frame
