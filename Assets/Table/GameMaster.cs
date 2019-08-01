08dad9ddb9a5d59fa0a79c7dceeb4745631427fb
cabakon <ondrej@cabak.cz>
2019-08-01 20:10:27 +0200

    zobrazení Score

--- Assets/Table/GameMaster.cs
+++ Assets/Table/GameMaster.cs
@@ -1,19 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMaster : MonoBehaviour
 {
     public int Score;
     public bool IsWinner = false;
+    private Text txtScoreTextComponent;
 
-    private void Update()
+    private void Start()
+    {   
+        GameObject txtScoreObject = GameObject.Find("txtScore");
+        if (txtScoreObject != null)
+        {
+            txtScoreTextComponent = txtScoreObject.GetComponent<Text>();
+            print(txtScoreTextComponent.text);
+            txtScoreTextComponent.text = "0";
+        }
+
+    }
+private void Update()
     {
         if(!IsWinner && Score >= 5)
         {
             print("Výhra");
             IsWinner = true;
-        }      
+        }
 
+        txtScoreTextComponent.text = Score.ToString();
     }
+
 }
